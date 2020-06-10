8f50287fbaa4fe5c18536eae11e08e2343b1e593
Thiago <thiago.santana1@aluno.ufop.edu.br>
2020-06-10 12:29:11 -0300

    Versão Inicial

---
+++ src/App.js
@@ -0,0 +1,44 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
+/* eslint-disable jsx-a11y/alt-text */
+
+import React, { Component } from 'react';
+import './style.css';
+
+export default class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      nutri: []
+    };
+  }
+  componentDidMount(){
+    let url = 'https://sujeitoprogramador.com/rn-api/?api=posts';
+    fetch(url).then((r) => r.json()).then((json) =>{
+      let state = this.state;
+      state.nutri = json;
+      this.setState(state);
+      //console.log(state);
+    });
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <header>
+          <strong>React Nutri</strong>
+        </header>
+        {this.state.nutri.map((item)=>{
+          return(
+            <article key={item.id} className="post">
+              <strong className="titulo">{item.titulo}</strong>
+              <img src={item.capa} className="capa"/>
+              <p className="subtitulo">{item.subtitulo}</p>
+              <a className="botao" href="#"> Veja Mais</a>
+            </article>
+          );
+        })}
+      </div>
+    );
+  }
+}
